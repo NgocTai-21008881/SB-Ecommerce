cc1199f045ad8eaa181939bcab3cc1bcfb90e689
TAINGUYENNGOC\LENOVO <nguyenngoctai201202@gmail.com>
2025-06-04 23:25:46 +0700

    update throw delete category

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +27,11 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId)).findFirst().orElse(null);
-        if (category == null)
-            return "Category not found";
-        
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Category not found"));
+
         categories.remove(category);
         return "Deleted category with id " + categoryId + " success";
     }
