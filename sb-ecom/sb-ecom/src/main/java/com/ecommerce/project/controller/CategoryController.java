953bca2dcf964dd61447840d37cca0f8de9be8ad
TAINGUYENNGOC\LENOVO <nguyenngoctai201202@gmail.com>
2025-06-05 15:52:41 +0700

    update CategoryServiceImpl using JpaRepository

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 public class CategoryController {
     private CategoryService categoryService;
 
@@ -20,19 +21,21 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("api/public/categories")
+    //@GetMapping("/public/categories")
+    @RequestMapping(value = "/public/categories", method = RequestMethod.GET)
     public ResponseEntity<List<Category>> getAllCategories() {
         List<Category> categories = categoryService.getAllCategories();
         return new ResponseEntity<>(categories,HttpStatus.OK);
     }
 
-    @PostMapping("api/admin/categories")
+    //@PostMapping("/api/admin/categories")
+    @RequestMapping(value = "/admin/categories", method = RequestMethod.POST)
     public ResponseEntity<String> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category added successfully",HttpStatus.CREATED);
     }
 
-    @DeleteMapping("api/admin/categories/{categoryId}")
+    @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
         try {
             String status = categoryService.deleteCategory(categoryId);
@@ -45,7 +48,7 @@ public class CategoryController {
 
     }
 
-    @PutMapping("api/admin/categories/{categoryId}")
+    @PutMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId) {
         try {
             Category savedCategory = categoryService.updateCategory(category, categoryId);
--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/model/Category.java
@@ -1,9 +1,15 @@
 package com.ecommerce.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.springframework.stereotype.Component;
 
-
+@Entity(name = "categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 
@@ -30,4 +36,5 @@ public class Category {
     public void setCategoryName(String categoryName) {
         this.categoryName = categoryName;
     }
+
 }
