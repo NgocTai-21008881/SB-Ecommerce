800e2def91c356b0e7565a7fdb834e3b0bba3e59
TAINGUYENNGOC\LENOVO <nguyenngoctai201202@gmail.com>
2025-06-06 20:19:54 +0700

    update get/set mapping product

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -17,10 +17,12 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
     private String productName;
+    private String image;
     private String description;
     private int quantity;
-    private double price;
-    private double specialPrice;
+    private double price; //100
+    private double discount; //25
+    private double specialPrice; //75
 
     @ManyToOne
     @JoinColumn(name = "category_id")
--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/payload/ProductDTO.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -1,12 +1,12 @@
 package com.ecommerce.project.payload;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class ProductDTO {
     private Long productId;
     private String description;
