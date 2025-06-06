4a27cb33545d195f0f74789610fc132b9ba906ef
TAINGUYENNGOC\LENOVO <nguyenngoctai201202@gmail.com>
2025-06-06 20:34:38 +0700

    add get products by category

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/service/ProductService.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/service/ProductService.java
@@ -9,4 +9,6 @@ public interface ProductService {
     ProductDTO addProduct(Long categoryId, Product product);
 
     ProductResponse getAllProduct();
+
+    ProductResponse searchByCategory(Long categoryId);
 }
