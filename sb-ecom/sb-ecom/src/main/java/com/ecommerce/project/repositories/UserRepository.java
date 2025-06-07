32798a369ad0ccff62cd1773454484adc369196b
TAINGUYENNGOC\LENOVO <nguyenngoctai201202@gmail.com>
2025-06-08 00:28:16 +0700

    implement sigup authController

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/repositories/UserRepository.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +12,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserName(String username);
+
+    Boolean existsByUserName(@NotBlank @Size(min = 3, max = 20) String username);
+
+    Boolean existsByEmail(@NotBlank @Size(max = 50) @Email String email);
 }
